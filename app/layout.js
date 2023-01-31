50876e4f2c4a95333039e967780f0e2f5a868629
muhammad-jiku <muhammadjiku364@gmail.com>
2023-01-31 23:31:35 +0600

    NextAuth implemented

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,18 @@
 import Footer from '@/components/Shared/Footer/Footer';
 import Navbar from '@/components/Shared/Navbar/Navbar';
 import '../styles/globals.css';
+import Provider from './provider';
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head />
       <body className="flex flex-col min-h-screen">
-        <Navbar />
-        {children}
-        <Footer />
+        <Provider>
+          <Navbar />
+          {children}
+          <Footer />
+        </Provider>
       </body>
     </html>
   );
