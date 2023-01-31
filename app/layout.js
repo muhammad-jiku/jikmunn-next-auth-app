f7991fa71ef364232e04201a6a6d001398ad1073
muhammad-jiku <muhammadjiku364@gmail.com>
2023-01-31 19:25:09 +0600

    layout

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head />
-      <body>{children}</body>
+      <body className="flex flex-col min-h-screen">{children}</body>
     </html>
   );
 }
