c50b167cbc7bc2459cde4b31450129788cce9b77
muhammad-jiku <muhammadjiku364@gmail.com>
2023-02-01 19:04:02 +0600

    mongoose schema added

---
+++ db/schema/userSchema.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+
+const userSchema = mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, 'Please provide your username'],
+      trim: true,
+      minLength: [3, 'Username must be at least 3 letters'],
+      maxLength: [100, 'Username is too long'],
+    },
+    email: {
+      type: String,
+      required: [true, 'Please provide your email'],
+      unique: [true, 'This user is already in!'],
+    },
+    password: {
+      type: String,
+      minLength: [6, 'Password must be at least 3 letters'],
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = userSchema;
