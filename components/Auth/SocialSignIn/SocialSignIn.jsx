6b6a79be570613fd261cae5f0d6185b318b93cb7
muhammad-jiku <muhammadjiku364@gmail.com>
2023-02-01 22:00:32 +0600

    some modification for assets

--- components/Auth/SocialSignIn/SocialSignIn.jsx
+++ components/Auth/SocialSignIn/SocialSignIn.jsx
@@ -3,6 +3,7 @@
 import React from 'react';
 import googleLogo from '../../../assets/images/google.png';
 import githubLogo from '../../../assets/images/github.png';
+import Image from 'next/image';
 
 const SocialSignIn = () => {
   const handleGoogleLogin = async () => {
@@ -17,13 +18,29 @@ const SocialSignIn = () => {
         className="btn btn-outline btn-primary w-full my-2"
         onClick={handleGoogleLogin}
       >
-        Continue with <img src={googleLogo} alt="" className="ml-2" />{' '}
+        Continue with{' '}
+        <img
+          src={googleLogo.src}
+          //   src={`${googleLogo}`}
+          alt="google"
+          //   width={100}
+          //   height={100}
+          className="ml-2"
+        />{' '}
       </button>{' '}
       <button
         className="btn btn-outline btn-primary w-full my-2"
         onClick={handleGithubLogin}
       >
-        Continue with <img src={githubLogo} alt="" className="ml-2" />{' '}
+        Continue with{' '}
+        <img
+          src={githubLogo.src}
+          //   src={`${githubLogo}`}
+          alt="github"
+          //   width={100}
+          //   height={100}
+          className="ml-2"
+        />{' '}
       </button>
     </div>
   );
