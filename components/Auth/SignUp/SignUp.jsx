5c39a41d63aae3422bc417eb850199e77688c717
muhammad-jiku <muhammadjiku364@gmail.com>
2023-02-01 12:57:15 +0600

    sign up modified

--- components/Auth/SignUp/SignUp.jsx
+++ components/Auth/SignUp/SignUp.jsx
@@ -3,108 +3,173 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
 
 import { HiAtSymbol, HiFingerPrint, HiOutlineUser } from 'react-icons/hi';
 
 const SignUp = () => {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+    watch,
+    reset,
+  } = useForm();
+
+  let errorMessage;
+
+  const onSubmit = async () => {
+    const name = watch('name').toUpperCase();
+    const email = watch('email');
+    const password = watch('password');
+
+    const newUser = {
+      name: name,
+      email: email,
+      password: password,
+    };
+
+    console.log(newUser);
+    reset();
+  };
+
   const [show, setShow] = useState({ password: false, cpassword: false });
   return (
-    <div>
+    <>
       <Head>
         <title>Sign Up</title>
       </Head>
+      <div className="hero min-h-screen">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          {/* <div className="text-center lg:text-left">
+            <h1 className="text-5xl font-bold">Registration now!</h1>
+            <p className="py-6">
+              Welcome to the Jikmunn Billing. Here you can track your billing
+              services cost. So, Registration now to start your journey!
+            </p>
+          </div> */}
+          <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl">
+            <div className="card-body">
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="form-control mb-4">
+                  <label className="label">
+                    <span className="label-text text-primary font-bold">
+                      Name
+                    </span>
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="Full Name"
+                    className="input input-bordered input-primary"
+                    {...register('name', {
+                      required: {
+                        value: true,
+                        message: 'Full Name is required',
+                      },
+                      // maxLength: {
+                      //   value: 20,
+                      //   message: 'Name can not be more than 20 letters',
+                      // },
+                    })}
+                    style={{ backgroundColor: 'white' }}
+                  />
+                  <p className="text-red-500 font-semibold">
+                    {errors?.displayName?.type === 'required' && (
+                      <span>{errors?.displayName?.message}</span>
+                    )}
+                    {/* {errors?.displayName?.type === 'maxLength' && (
+                    <span>{errors?.displayName?.message}</span>
+                  )} */}
+                  </p>
+                </div>
 
-      <section className="w-3/4 mx-auto flex flex-col gap-10">
-        <div className="title">
-          <h1 className="text-gray-800 text-4xl font-bold py-4">Sign Up</h1>
-          <p className="w-3/4 mx-auto text-gray-400">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores,
-            officia?
-          </p>
+                <div className="form-control mb-4">
+                  <label className="label">
+                    <span className="label-text text-primary font-bold">
+                      Email
+                    </span>
+                  </label>
+                  <input
+                    type="email"
+                    placeholder="email"
+                    className="input input-bordered input-primary"
+                    {...register('email', {
+                      required: {
+                        value: true,
+                        message: 'Email is required',
+                      },
+                      pattern: {
+                        value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
+                        message: 'Invalid Email',
+                      },
+                    })}
+                    style={{ backgroundColor: 'white' }}
+                  />
+                  <p className="text-red-500 font-semibold">
+                    {errors.email?.type === 'required' && (
+                      <span>{errors?.email?.message}</span>
+                    )}
+                    {errors.email?.type === 'pattern' && (
+                      <span>{errors?.email?.message}</span>
+                    )}
+                  </p>
+                </div>
+
+                <div className="form-control mb-4">
+                  <label className="label">
+                    <span className="label-text text-primary font-bold">
+                      Password
+                    </span>
+                  </label>
+                  <input
+                    type="password"
+                    placeholder="password"
+                    className="input input-bordered input-primary"
+                    {...register('password', {
+                      required: {
+                        value: true,
+                        message: 'Password is required',
+                      },
+                      minLength: {
+                        value: 6,
+                        message: 'Password must be at least six letters',
+                      },
+                    })}
+                    style={{ backgroundColor: 'white' }}
+                  />
+                  <p className="text-red-500 font-semibold">
+                    {errors.password?.type === 'required' && (
+                      <span>{errors?.password?.message}</span>
+                    )}
+                    {errors.password?.type === 'minLength' && (
+                      <span>{errors?.password?.message}</span>
+                    )}
+                  </p>
+                </div>
+
+                <div className="form-control mt-6">
+                  {errorMessage}
+                  <input
+                    type="submit"
+                    className="btn btn-primary text-white uppercase"
+                    value="Registration"
+                  />{' '}
+                  <p className="text-center font-bold">
+                    Already have an account?
+                    <Link href={`/sign-in`}>
+                      <span className="text-primary cursor-pointer">
+                        {' '}
+                        sign in now!
+                      </span>
+                    </Link>
+                  </p>
+                </div>
+              </form>
+            </div>
+          </div>
         </div>
-
-        {/* form */}
-        <form className="flex flex-col gap-5">
-          <div
-          // className={styles.input_group}
-          >
-            <input
-              type="text"
-              name="Username"
-              placeholder="Username"
-              // className={styles.input_text}
-            />
-            <span className="icon flex items-center px-4">
-              <HiOutlineUser size={25} />
-            </span>
-          </div>
-          <div
-          // className={styles.input_group}
-          >
-            <input
-              type="email"
-              name="email"
-              placeholder="Email"
-              // className={styles.input_text}
-            />
-            <span className="icon flex items-center px-4">
-              <HiAtSymbol size={25} />
-            </span>
-          </div>
-          <div
-          // className={styles.input_group}
-          >
-            <input
-              type={`${show.password ? 'text' : 'password'}`}
-              name="password"
-              placeholder="password"
-              // className={styles.input_text}
-            />
-            <span
-              className="icon flex items-center px-4"
-              onClick={() => setShow({ ...show, password: !show.password })}
-            >
-              <HiFingerPrint size={25} />
-            </span>
-          </div>
-
-          <div
-          // className={styles.input_group}
-          >
-            <input
-              type={`${show.cpassword ? 'text' : 'password'}`}
-              name="cpassword"
-              placeholder="Confirm Password"
-              // className={styles.input_text}
-            />
-            <span
-              className="icon flex items-center px-4"
-              onClick={() => setShow({ ...show, cpassword: !show.cpassword })}
-            >
-              <HiFingerPrint size={25} />
-            </span>
-          </div>
-
-          {/* login buttons */}
-          <div className="input-button">
-            <button
-              type="submit"
-              // className={styles.button}
-            >
-              Login
-            </button>
-          </div>
-        </form>
-
-        {/* bottom */}
-        <p className="text-center text-gray-400 ">
-          Have an account?{' '}
-          <Link href={'/sign-in'}>
-            <span className="text-blue-700">Sign In</span>
-          </Link>
-        </p>
-      </section>
-    </div>
+      </div>
+    </>
   );
 };
 
