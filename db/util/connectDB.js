e253bb23b425815b6d1567bce2be4c26d9a75b94
muhammad-jiku <muhammadjiku364@gmail.com>
2023-02-01 19:17:09 +0600

    db connect function added

---
+++ db/util/connectDB.js
@@ -0,0 +1,20 @@
+const { default: mongoose } = require('mongoose');
+
+const uri = `mongodb+srv://${process.env.DB_AUTHOR}:${process.env.DB_PASS}@cluster0.nstalsd.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+const databaseConnect = () => {
+  mongoose.set('strictQuery', false);
+  mongoose
+    .connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log('DB connected!!');
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+module.exports = databaseConnect;
