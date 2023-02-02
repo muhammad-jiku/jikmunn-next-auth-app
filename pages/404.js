47632862ca3f3ce1c19f349ec40faa98289b3e19
muhammad-jiku <muhammadjiku364@gmail.com>
2023-02-02 22:19:17 +0600

    404 implemented

---
+++ pages/404.js
@@ -0,0 +1,12 @@
+import NotFound from '@/components/Shared/NotFound/NotFound';
+import React from 'react';
+
+const NotFoundPage = () => {
+  return (
+    <>
+      <NotFound />
+    </>
+  );
+};
+
+export default NotFoundPage;
